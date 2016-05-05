343cfbc07b5f53fbf14412c71078259ff8ec6190
GamexStudio <vyas.darshan2052@gmail.com>
2016-05-05 18:56:46 +0530

    Added files via upload

---
+++ VIdeoDownloadGamex-master/FlipSample/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  FlipSample
+//
+//  Created by TheTiger on 05/05/16.
+//  Copyright © 2016 TheTiger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ VIdeoDownloadGamex-master/FlipSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlipSample
+//
+//  Created by TheTiger on 05/05/16.
+//  Copyright © 2016 TheTiger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
